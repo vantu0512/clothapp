5465ea42ca135116477767b3e2a1e6f82d08a99b
HP <HP@DESKTOP-3LTH0II>
2021-06-01 16:20:53 +0700

    lab

--- Web/config.php
+++ Web/config.php
@@ -1,13 +1,14 @@
 <?php
-$conn_string = "host=ec2-54-211-176-156.compute-1.amazonaws.com"
-        . " port=5432 dbname=d7k35e83nt54kf"
-        . " user=lkblfujpnveoph"
-        . " password=a2dda41d2a64a9bc1de4ccc447742ff9c32185457810c259326670ce98c0fc42";
+$conn_string = "host=ec2-107-21-10-179.compute-1.amazonaws.com"
+        . " port=5432 "
+        . " dbname=dca4nl249vqnhv"
+        . " user=wcgxbroqcssxxa"
+        . " password=e0c98a99046235cf276d2e104f8f4305dcfce626c2dc4aeb7d83b969234dd912";
 $db = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
    if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+      echo "Opened database successfully Tu \n";
    }
 ?>
